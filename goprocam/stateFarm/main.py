ae5abf5d0616b868f7002f92445b5605cd9b3f81
armel50 <armelfli50@gmail.com>
2022-11-02 17:45:45 -0500

    the user can now download all the file and move it to a specific folder

--- goprocam/stateFarm/main.py
+++ goprocam/stateFarm/main.py
@@ -5,11 +5,12 @@
 from goprocam import GoProCamera, constants
 import shutil
 import asyncio
+donwloaded_picture = 0 
+downloaded_video = 0
 
 # create a Go Pro object
 go_pro = GoProCamera.GoPro()
-donwloaded_picture = 0 
-downloaded_video = 0
+
 
 
 def show_all_my_media():
@@ -21,14 +22,15 @@ def delete_media(file="all"):
 def download_all_media_to_image_folder(): # This function will dowload all the media on the Go Pro
     all_media = go_pro.downloadAll()
     for file in all_media:
-        shutil.move('./{}'.format(file), './images/{}'.format(file))
+        shutil.move('./100GOPRO-{}'.format(file), './images/100GOPRO-{}'.format(file))
     delete_media()
 
 
 def take_a_picture(timer_input=0, timelapse_activated = False): 
+    global donwloaded_picture
     if timelapse_activated:
         i = 0
-        while i <10: # Take a picture every {timer_input} seconds
+        while i <20: # Take a picture every {timer_input} seconds
             go_pro.take_photo(timer=timer_input)
             filename = 'timelapse_photo_{}.JPG'.format(i) 
             go_pro.downloadLastMedia(custom_filename=filename)
@@ -36,7 +38,7 @@ def take_a_picture(timer_input=0, timelapse_activated = False):
             # Move the downloded file to the folder your choice
             shutil.move('./{}'.format(filename), './images/{}'.format(filename))
             i+=1
-    else:
+    else: # Take only one picture
         go_pro.take_photo(timer=timer_input)
         filename= 'photo_{}.JPG'.format(donwloaded_picture)
         go_pro.downloadLastMedia(custom_filename=filename)
@@ -46,6 +48,7 @@ def take_a_picture(timer_input=0, timelapse_activated = False):
         donwloaded_picture += 1
 
 def take_a_video(duration_input=5):
+    global downloaded_video
     go_pro.shoot_video(duration=duration_input)
     filename= 'video_{}.mp4'.format(downloaded_video)
     go_pro.downloadLastMedia(custom_filename=filename)
@@ -57,7 +60,7 @@ def take_a_video(duration_input=5):
     
 
 
-# take_a_picture(0.5,True)
-take_a_video()
+# take_a_picture(3,False)
+# take_a_video()
 
-# download_all_media_to_image_folder()
+download_all_media_to_image_folder()
